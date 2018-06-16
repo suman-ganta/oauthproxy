14abb5fcf19842c3c556b25c0f8e74d478732435
Suman Ganta <suman.g.ganta@oracle.com>
2018-06-16 12:56:41 -0700

    embed image

--- PITCHME.md
+++ PITCHME.md
@@ -16,9 +16,8 @@ https://t1.sumanganta.com/eng/ic/api/process/v1/dp-executions
 
 ### Host Resolution
 
-| --------|--------------|------------------|--------------------|
-|Root DNS | OCI DNS Zone | Tenant Subdomain | Ingress Controller |
-| --------|--------------|------------------|--------------------|
+![graph LR RootDNS(Root DNS) --> DNSZone(DNS Zone) subgraph OCI DNSZone --> TenantDomain(Tenant Domain) end](https://mermaidjs.github.io/mermaid-live-editor/#/view/eyJjb2RlIjoiZ3JhcGggTFJcbiAgICBSb290RE5TKFJvb3QgRE5TKSAtLT4gRE5TWm9uZShETlMgWm9uZSlcbiAgICBzdWJncmFwaCBPQ0lcbiAgICBETlNab25lIC0tPiBUZW5hbnREb21haW4oVGVuYW50IERvbWFpbilcbiAgICBlbmRcbiIsIm1lcm1haWQiOnsidGhlbWUiOiJkZWZhdWx0In19)
+<img src="https://mermaidjs.github.io/mermaid-live-editor/#/view/eyJjb2RlIjoiZ3JhcGggTFJcbiAgICBSb290RE5TKFJvb3QgRE5TKSAtLT4gRE5TWm9uZShETlMgWm9uZSlcbiAgICBzdWJncmFwaCBPQ0lcbiAgICBETlNab25lIC0tPiBUZW5hbnREb21haW4oVGVuYW50IERvbWFpbilcbiAgICBlbmRcbiIsIm1lcm1haWQiOnsidGhlbWUiOiJkZWZhdWx0In19">![Al
 +++
 
 ### TLS
--- src/main/java/oracle/oauthproxy/Options.java
+++ src/main/java/oracle/oauthproxy/Options.java
@@ -16,14 +16,17 @@ public class Options {
   public Integer port = 8090;
 
   @Option(names = {"-i", "--issuer"}, description = "Token issuer url, OpenID issuer url")
-  public String issuer = "http://127.0.0.1:5556/dex";
+  //public String issuer = "http://127.0.0.1:5556/dex";
+  public String issuer = "https://idcs-4887c2ab59484d4b92e4cb4bd111891b.identity.oraclecloud.com/";
 
   @Option(names = {"-ci", "--clientId"}, description = "Client Id of OAuth client")
-  public String clientId = "oic-proxy-app";
+  //public String clientId = "oic-proxy-app";
+  public String clientId = "5b17a6c1328a4ac1b95841c0d8497bc3";
 
   @Option(names = {"-cs", "--clientSecret"}, description = "Client Secret of OAuth client")
-  public String clientSecret = "mysecretoicclient";
+  //public String clientSecret = "mysecretoicclient";
+  public String clientSecret = "0ed1e8a3-fc29-40da-9b71-da026ff8cf2d";
 
   @Option(names = {"-cb", "--callback"}, description = "Callback URI")
-  public String redirectUri = "http://127.0.0.1:8089/p/callback";
+  public String redirectUri = "https://www.sumanganta.com/p/p/callback";
 }
